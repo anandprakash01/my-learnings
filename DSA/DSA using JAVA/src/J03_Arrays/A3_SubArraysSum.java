fc7531ba1f6c0763acb0ff04ebc4082d838856cd
anandprakash01 <ap.anandprakash21@gmail.com>
2025-04-27 01:57:55 +0530

    java

--- DSA/DSA using JAVA/src/J03_Arrays/A3_SubArraysSum.java
+++ DSA/DSA using JAVA/src/J03_Arrays/A3_SubArraysSum.java
@@ -3,11 +3,14 @@ package J03_Arrays;
 public class A3_SubArraysSum {
 
     public static void allPairs(int arr[]){//contiguous pairs
+        int total=0;
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++){
                 System.out.print("("+ arr[i]+ ","+ arr[j]+"), ");
+                total++;
             }
         }
+        System.out.println("Total no of Pairs = "+total);
     }
     public static void PrintallSubarrays(int a[]){
         int ts=0;
@@ -27,9 +30,9 @@ public class A3_SubArraysSum {
     public static void main(String args[]){
         int a[]={3,2,6,1,5};
         int b[]={1,-2,6,-1,-3};
-        allPairs(a);
+//        allPairs(a);
 //        PrintallSubarrays(a);
-        //maxSubarraySum(b);
+//        maxSubarraySum(b);
     }
 }
 
